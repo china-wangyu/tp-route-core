d2438bb2c49a939cb328c9901aa85468fc3af6fa
china-wangyu <china_wangyu@aliyun.com>
2019-07-03 17:01:07 +0800

    '更新异常输出'

--- src/exception/RouteException.php
+++ src/exception/RouteException.php
@@ -6,24 +6,9 @@
 namespace WangYu\exception;
 
 
-class RouteException extends \Exception
+class RouteException extends \WangYu\BaseException
 {
     public $code = 400;
     public $message = '反射路由设置错误';
     public $error_code = 66668;
-
-    public function __construct($params = [])
-    {
-        isset($params['code']) && $this->code = $params['code'];
-        isset($params['message']) && $this->message = $params['message'];
-        isset($params['error_code']) && $this->error_code = $params['error_code'];
-        if(class_exists('\LinCmsTp5\exception\BaseException')){
-            throw  new \LinCmsTp5\exception\BaseException([
-                'code' => $this->code,
-                'msg' => $this->message,
-                'error_code' => $this->error_code,
-            ]);
-        }
-        parent::__construct($this->error_code.$this->message,$this->code);
-    }
 }
